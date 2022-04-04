c7ba11931e938fdc516d05f43d4723603606bcac
Jose Daniel Parra Gonzalez <jose-paper@hotmail.com>
2022-04-03 22:13:36 -0500

    Metodos publicos y provados

--- classes/basica.ts
+++ classes/basica.ts
@@ -4,14 +4,20 @@
         //public team : string ;
         //public realName? : string;
         static avAge: number = 35;
+        static getAvgAge() {return this.name}
 
         constructor(private name: string, 
             private team: string, 
             public realName?: string
             ) {}
+
+        public bio() {
+            return `${this.name} (${this.team})`
+        }
     }
 
     const antman: Avenger = new Avenger("Antman", "Capitan", "Scott Lang");
     console.log(antman);
-    console.log(Avenger);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })()
--- main.js
+++ main.js
@@ -6,10 +6,15 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() { return this.name; }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avAge = 35;
     const antman = new Avenger("Antman", "Capitan", "Scott Lang");
     console.log(antman);
-    console.log(Avenger);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
